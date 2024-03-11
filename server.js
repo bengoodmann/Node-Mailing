3b74297fd63cf890c8041a3cb3a24d10770de651
Benson "Santi" Isaac <oluchukwubensonisaac@gmail.com>
2024-03-11 02:57:39 +0100

    Using expressJS to send email from a nodejs server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const htmlPath = path.join(__dirname, "index.html");
 
-
+// sending from index.html
 fs.readFile(htmlPath, "utf8", (err, data) => {
   if (err) {
     console.error("Error reading HTML file:", err);
@@ -16,7 +19,7 @@ fs.readFile(htmlPath, "utf8", (err, data) => {
     // host: "smtp.gmail.com",
     // port: 587,
     // secure: false,
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: "dummyprogramtest@gmail.com",
       pass: process.env.pass,
@@ -39,3 +42,43 @@ fs.readFile(htmlPath, "utf8", (err, data) => {
     console.log("Message ID: " + info.messageId);
   });
 });
+
+// sending from a server
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/send-mail", (req, res) => {
+  const { subject, message } = req.body;
+
+  let tp = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "dummyprogramtest@gmail.com",
+      pass: process.env.pass,
+    },
+  });
+
+  let mailOptions = {
+    from: "'Bes'<dummyprogramtest@gmail.com>",
+    to: ["bensonoluchukwuisaac@gmail.com", "bensonsibigamisaac@gmail.com"],
+    subject: subject,
+    html: message,
+  };
+
+  tp.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error occurred: " + error.message);
+      res.status(500).json({ message: "An error occurred" });
+    } else {
+      console.log("Email sent successfully!");
+      console.log("Message ID: " + info.messageId);
+      res.status(200).json({ message: "Email sent successfully" });
+    }
+  })
+});
+
+app.listen(3000, () => {
+ console.log("Server started at port:3000")
+})
